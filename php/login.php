ab89f11c08673bfa70506e15705e75ecbfa60516
HoYeon_LEE <lsjkeh9683@gmail.com>
2024-06-07 09:36:35 +0900

    Update login.php

--- php/login.php
+++ php/login.php
@@ -6,15 +6,18 @@ $username = "dev";
 $password = "Phantom0219?";
 $database = "auth";
 
+// MySQL 연결
 $conn = new mysqli($servername, $username, $password, $database);
 
+// 연결 실패 시 오류 처리
 if ($conn->connect_error) {
     die("MySQL 연결 실패: " . $conn->connect_error);
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['id'];
-    $password = $_POST['password'];
+    // 입력된 패스워드를 SHA-256으로 해싱
+    $password = hash('sha256', $_POST['password']);
 
     // 로그인 로직 처리
     $sql = "SELECT * FROM member WHERE username = ?";
@@ -25,6 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
+        // 데이터베이스에 저장된 해시된 비밀번호와 입력된 해시된 비밀번호 비교
         if ($row['password'] === $password) {
             // 로그인 성공 시 세션 활성화
             $_SESSION['user_id'] = $row['id']; // 사용자 ID를 세션에 저장
@@ -43,12 +47,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 // 세션 유효 시간 제한 (30분)
-if (isset($_SESSION['login_time']) && time() - $_SESSION['login_time'] > 1800) { 
+if (isset($_SESSION['login_time']) && time() - $_SESSION['login_time'] > 1800) {
     // 세션 만료 시 로그아웃 처리
     session_unset();
     session_destroy();
 }
 
-$stmt->close();
+// 리소스 해제
+if (isset($stmt)) {
+    $stmt->close();
+}
 $conn->close();
 ?>
